0290784d026e17a513e294989fc19dffc8cc9e48
Serhii Stanislav <serhiistanislav@gmail.com>
2023-05-15 22:57:43 +0300

    ADD | data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+import dataJson from './data.json';
 
 function App() {
+  const data = dataJson;
+
+  console.log('data -->', data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>{data.name}</header>
     </div>
   );
 }
