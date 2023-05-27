6e65133574cb5ceb37b9e6ea29bd07bf801b03e0
Serhii Stanislav <serhiistanislav@gmail.com>
2023-05-27 22:17:18 +0300

    EDIT | code refactoring

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,6 @@
-import { Helmet } from 'react-helmet-async';
-
 const HomePage = () => {
   return (
     <>
-      <Helmet>
-        <title>Learning React Helmet!</title>
-        <meta
-          name='description'
-          content='Beginner friendly page for learning React Helmet.'
-        />
-        <meta property='og:type' content='article111' />
-        <meta property='og:title' content='Learning React Helmet!' />
-        <meta
-          property='og:description'
-          content='Beginner friendly page for learning React Helmet.'
-        />
-        {/* End Facebook tags */}
-        {/* Twitter tags */}
-      </Helmet>
       <h1>Home page</h1>
     </>
   );
